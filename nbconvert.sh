0a3c9db545ca5bb18786169f395efc5c7c9dfa13
ncho-sqd <cjw.neil@gmail.com>
2018-10-14 21:32:47 -0500

    Completed bash script to automatically convert jupyter notebook to markdown and move images files out.

--- nbconvert.sh
+++ nbconvert.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # capture input passed to bash script
 FILE="$1"
 
@@ -6,8 +8,19 @@ FILENAME=`basename $FILE .ipynb`
 NEW_FILENAME=`date "+%Y-%m-%d-"`${FILENAME}
 
 # construct markdown filename and dirname included images
-POST_NAME="${FILENAME}.md"
-IMG_DIR="${FILENAME}_files"
+POST_NAME="${NEW_FILENAME}.md"
+IMG_DIR="${NEW_FILENAME}_files"
  
 # call nbconvert
 jupyter nbconvert --to markdown $FILE --output-dir ~/git/blog/_posts --output $NEW_FILENAME
+
+# rename image files in markdown to moved /images folder (to be moved)
+for fp in ~/git/blog/_posts/$IMG_DIR/*; do
+    FN=$IMG_DIR/`basename $fp`;
+    #echo /images/$FN;
+    sed -i '' "s#$FN#/images/$FN#g" ~/git/blog/_posts/$POST_NAME;
+done
+
+# move image folder automatically created under _post to /image folder
+rsync -a ~/git/blog/_posts/${IMG_DIR} ~/git/blog/images
+rm -rf ~/git/blog/_posts/${IMG_DIR}
